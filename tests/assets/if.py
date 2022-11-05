0f45e82b4310c6dd9cacea0cce1bb4f3024a4a60
kenkoooo <kenkou.n@gmail.com>
2022-11-06 00:01:29 +0900

    add: integration test generator

--- tests/assets/if.py
+++
@@ -1,6 +0,0 @@
-a, b = map(int, input().split())
-ans = a * b
-if ans % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,12 +1,29 @@
 mod test_env;
-use test_env::execute;
 
-#[test]
-fn test_if_integration_test() -> anyhow::Result<()> {
-    let code = include_str!("./assets/if.py");
+#[macro_export]
+macro_rules! optpy_integration_test {
+    ($name:ident, $code:expr, $(($input:expr, $output:expr)),+) => {
+        #[test]
+        fn $name() {
+            let code = $code;
+            let tests = [$(($input, $output)),+];
+            for (input, output) in tests {
+                assert_eq!(test_env::execute(code, input).unwrap(), output);
+            }
+        }
+    };
+}
 
-    assert_eq!(execute(code, "3 4\n")?, "Even\n");
-    assert_eq!(execute(code, "3 5\n")?, "Odd\n");
-
-    Ok(())
+optpy_integration_test! {
+test_if_statement,
+r#"
+a, b = map(int, input().split())
+ans = a * b
+if ans % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+"#,
+("3 4\n", "Even\n"),
+("3 5\n", "Odd\n")
 }
