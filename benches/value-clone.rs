15bf6f810727c8e7773857672f6705b48abbb1f8
kenkoooo <kenkou.n@gmail.com>
2022-12-11 05:50:45 +0900

    add bench

---
+++ benches/value-clone.rs
@@ -0,0 +1,28 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use optpy_runtime::Value;
+
+fn value_clone(c: &mut Criterion) {
+    c.bench_function("clone list value", |b| {
+        b.iter(|| {
+            let x = Value::from(vec![]);
+            for _ in 0..100000 {
+                let _ = x.clone();
+            }
+        })
+    });
+    c.bench_function("clone raw number", |b| {
+        b.iter(|| {
+            let x = Value::from(vec![]);
+            let x = match x {
+                Value::List(x) => x,
+                _ => unreachable!(),
+            };
+            for _ in 0..100000 {
+                let _ = x.clone();
+            }
+        })
+    });
+}
+
+criterion_group!(benches, value_clone);
+criterion_main!(benches);
